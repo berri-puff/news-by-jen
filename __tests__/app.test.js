2997816c2341d6f487572a3dcfc114310230ebe4
berri-puff <jennyzhang0207@gmail.com>
2023-11-20 14:27:05 +0000

    gets all endpoints information from /api

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,7 @@ const {
   topicData,
   userData,
 } = require("../db/data/test-data/index");
+const endpoints = require('../endpoints.json')
 
 beforeEach(() => {
   return seed({ articleData, commentData, topicData, userData });
@@ -33,6 +34,17 @@ describe("/api/topics", () => {
       });
   });
 });
+
+describe('/api', ()=>{
+  test('GET 200: responds with available api endpoints', ()=>{
+    return request(app)
+    .get('/api')
+    .expect(200)
+    .then(({body})=>{
+      expect(body.endpoints).toEqual(endpoints)
+    })
+  })
+})
 
 describe('Error Handling', ()=>{
     test('ERROR: 400 when given an invalid pathway', ()=>{
