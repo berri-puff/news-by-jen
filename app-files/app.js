72cbc57f440e88ea9eba8a44c7f2e5c58bacd977
berri-puff <jennyzhang0207@gmail.com>
2023-11-24 15:38:41 +0000

    separates users in own router file

--- app-files/app.js
+++ app-files/app.js
@@ -6,20 +6,14 @@ const {
   customErrors,
   psqlErrors,
 } = require(`${__dirname}/./errors`);
-const { getsArticle, getsAllArticles, getArticleComments, postsNewComment, patchArticleVote, postNewArticle  } = require(`${__dirname}/../controllers/article-controller`);
+const { postsNewComment, patchArticleVote, postNewArticle  } = require(`${__dirname}/../controllers/article-controller`);
 
 const { deletesComment, patchCommentVote } = require(`${__dirname}/../controllers/comments-controller`);
-const { getsUsers, getUserInfo } = require(`${__dirname}/../controllers/user-controller`);
 
 const app = express();
 app.use(express.json());
 
 app.use('/api', apiRouter);
-
-// app.get('/api/articles/:article_id/comments', getArticleComments)
-
-app.get('/api/users', getsUsers)
-app.get('/api/users/:username', getUserInfo)
 
 app.patch('/api/articles/:article_id', patchArticleVote)
 app.patch('/api/comments/:comment_id', patchCommentVote)
--- routes/api-router.js
+++ routes/api-router.js
@@ -1,3 +1,5 @@
+const usersRouter = require(`${__dirname}/./users-router`);
+
 const articlesRouter = require(`${__dirname}/./articles-router`);
 
 const topicsRouter = require(`${__dirname}/./topics-router`);
@@ -9,5 +11,6 @@ const apiRouter = require('express').Router();
 apiRouter.get('/', getsAllApi)
 apiRouter.use('/topics', topicsRouter)
 apiRouter.use('/articles', articlesRouter)
+apiRouter.use('/users', usersRouter)
 
 module.exports = apiRouter
