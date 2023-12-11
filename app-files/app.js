65a41af80c61feda33bf17dd5528a2ca02d91996
berri-puff <jennyzhang0207@gmail.com>
2023-12-11 10:20:48 +0000

    removes .only and corrects app.use

--- app-files/app.js
+++ app-files/app.js
@@ -7,8 +7,9 @@ const {
   psqlErrors,
 } = require(`${__dirname}/./errors`);
 const cors = require('cors');
-app.use(cors());
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
